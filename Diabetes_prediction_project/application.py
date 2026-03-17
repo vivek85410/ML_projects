caeb3a3752977965984dbbf703a81ec80f443370
vivek kumar <104557123+vivek85410@users.noreply.github.com>
2026-03-17 23:25:59 +0530

    Update application.py

--- Diabetes_prediction_project/application.py
+++ Diabetes_prediction_project/application.py
@@ -47,5 +47,5 @@ def predict_datapoint():
 def contact():
     return render_template('contact.html')
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+#if __name__=="__main__":
+#    app.run(host="0.0.0.0")
